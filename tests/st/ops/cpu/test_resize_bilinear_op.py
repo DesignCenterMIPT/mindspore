3f03545888acf8e083a18a93bee4545b61c57431
Alexander Gerasimov <samik.mechanic@gmail.com>
2023-11-14 19:39:30 +0300

    (test suite) add missing import

--- tests/st/ops/cpu/test_resize_bilinear_op.py
+++ tests/st/ops/cpu/test_resize_bilinear_op.py
@@ -14,6 +14,7 @@
 # ============================================================================
 import sys
 import numpy as np
+import pytest
 
 from mindspore import context, Tensor
 from mindspore.ops import operations as P
